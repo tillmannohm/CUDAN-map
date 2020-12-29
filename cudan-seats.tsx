f7965ca061812b2458269f257488512db5b6c428
TLMN <tillmann.ohm@tlu.ee>
2020-12-29 19:37:53 +0200

    clean

--- cudan-seats.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="cudan-seats" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="256" columns="16">
- <image source="cudan-seats.png" width="544" height="544"/>
-</tileset>
